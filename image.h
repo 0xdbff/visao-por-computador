bf0cd66d41762e39ee633ac416c2e21081d44841
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-05-08 01:32:51 +0100

    sync locals

--- image.h
+++ image.h
@@ -4,32 +4,20 @@
 extern "C" {
 #endif
 
+#include <assert.h>
 #include <ctype.h>
 #include <inttypes.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef loop
-#define loop for (;;)
-#endif
-
 #ifdef DEBUG
 #define LOG(...) printf(__VA_ARGS__)
-#define RETURN_NULL_ON_ERROR(msg) \
-    do {                          \
-        LOG(msg);                 \
-        if (file) fclose(file);   \
-        return NULL;              \
-    } while (0)
 #else
-#define RETURN_NULL_ON_ERROR(msg) \
-    do {                          \
-        if (file) fclose(file);   \
-        return NULL;              \
-    } while (0)
+#define LOG(...)
 #endif
 
 /// 8 bit RGB struct
@@ -71,7 +59,7 @@ typedef enum netpbm {
 } Netpbm;
 
 /// Image definition structure
-typedef struct image {
+typedef struct imageNpbm {
     /// Image ptr to first section of allocated data (byte)
     uint8_t* data;
     /// Image dimensions
@@ -80,11 +68,11 @@ typedef struct image {
     uint32_t max_value;
     /// Number of image channels
     uint8_t channels;
-    /// Bytes per pixel
-    uint8_t Bpp;
+    /// Bits per pixel
+    uint8_t bpp;
     /// Image type
     Netpbm type;
-} Image;  // 32 bytes struct
+} ImageNpbm;  // 32 bytes struct
 
 /// Return the number of bytes needed to store a channel
 static inline uint8_t bytes_per_channel(int precision) {
@@ -92,25 +80,48 @@ static inline uint8_t bytes_per_channel(int precision) {
 }
 
 /// Free an image allocation.
-static inline Image* free_image(Image* img) {
-    if (img) free(img);
+static inline ImageNpbm* free_image(ImageNpbm* img) {
+    if (img) {
+        if (img->data) free(img->data);
+        free(img);
+    }
     return NULL;
 }
 
 /// Allocate a new image.
-static inline Image* new_image(size_t width, size_t height, uint8_t channels,
-                               size_t precision, Netpbm type) {
-    Image* img = (Image*)malloc(sizeof(Image));
+static inline ImageNpbm* new_image(size_t width, size_t height,
+                                   uint8_t channels, size_t precision,
+                                   Netpbm type) {
+    ImageNpbm* img = (ImageNpbm*)malloc(sizeof(ImageNpbm));
+    img->data = NULL;
+    size_t data_size;
     if (img == NULL) return NULL;
 
     img->width = width;
     img->height = height;
     img->channels = channels;
     img->max_value = precision;
-    img->Bpp = bytes_per_channel(precision) * channels;
-    img->data = (uint8_t*)malloc(img->width * img->height * img->Bpp);
     img->type = type;
 
+    switch (type) {
+        case PBM:
+            data_size = (width * height + 7) / 8;
+            break;
+        case PGM:
+        case PPM:
+            data_size =
+                width * height * channels * bytes_per_channel(precision);
+            break;
+        case PAM:
+            data_size =
+                width * height * (channels + 1) * bytes_per_channel(precision);
+            break;
+        default:
+            free_image(img);
+            return NULL;
+    }
+
+    img->data = (uint8_t*)malloc(data_size);
     return img->data == NULL ? free_image(img) : img;
 }
 
@@ -140,44 +151,49 @@ static inline Netpbm eval_type(const char* signature) {
     else if (strcmp(signature, "P7") == 0)
         type = PAM;
     else {
-#ifdef DEBUG
         LOG("Not a valid Netpbm file!");
-#endif
     }
     return type;
 }
 
-static Image* pbm_img_from_fp(FILE* file) {
+static ImageNpbm* pbm_img_from_fp(FILE* file) {
+    ImageNpbm* img = NULL;
     uint32_t width, height;
 
     skip_whitespace_and_comments(file);
     if (fscanf(file, "%u", &width) != 1) {
-        RETURN_NULL_ON_ERROR("Not a valid pbm file!");
+        LOG("Not a valid pbm file!");
+        return img;
     }
     skip_whitespace_and_comments(file);
     if (fscanf(file, "%u", &height) != 1) {
-        RETURN_NULL_ON_ERROR("Not a valid pbm file!");
+        LOG("Not a valid pbm file!");
+        return img;
     }
     fgetc(file);
 
-    Image* img = new_image(width, height, 1, 1, PBM);
-    if (img == NULL) {
-        RETURN_NULL_ON_ERROR("Cannot allocate memory!");
+    img = new_image(width, height, 1, 1, PBM);
+    if (!img) {
+        LOG("Cannot allocate memory!");
+        return img;
     }
 
     size_t data_size = (img->width * img->height + 7) / 8;
     if (fread(img->data, 1, data_size, file) != data_size) {
-        RETURN_NULL_ON_ERROR("Not a valid pbm file!");
+        LOG("Not a valid pbm file!");
+        free_image(img);
+        return NULL;
     }
 
-    fclose(file);
     return img;
 }
 
 /// Read a image PBM, PGM, PPM, PAM
-static Image* read_image(const char* filepath) {
+static ImageNpbm* read_image(const char* filepath) {
     FILE* file = fopen(filepath, "rb");
     if (!file) return NULL;
+
+    ImageNpbm* img = NULL;
 
     // Read the header
     char signature[3] = {0};
@@ -185,24 +201,91 @@ static Image* read_image(const char* filepath) {
 
     // Read signature
     if (fscanf(file, "%2s", signature) != 1) {
-        RETURN_NULL_ON_ERROR("Not a valid pbm file!\nAborting!");
+        if (file) fclose(file);
+        LOG("Not a valid pbm file!\nAborting!");
+        return img;
     }
 
     Netpbm type = eval_type(signature);
     if (type == INVALID_TYPE) {
-        RETURN_NULL_ON_ERROR("Aborting!");
+        if (file) fclose(file);
+        LOG("Aborting!");
+        return img;
     }
 
     if (type == PBM) {
-        Image* img = pbm_img_from_fp(file);
+        img = pbm_img_from_fp(file);
         if (!img) {
-            return NULL;
+            if (file) fclose(file);
+            LOG("Aborting!");
+            return img;
         }
         return img;
     }
 
-    fclose(file);
+    if (file) fclose(file);
     return NULL;
+}
+
+// Function to write a PBM image to a file
+static uint8_t write_pbm_image(const char* filepath, const ImageNpbm* img) {
+    assert(img->type == PBM);
+
+    FILE* file = fopen(filepath, "wb");
+    if (!file) return false;
+
+    fprintf(file, "P4\n%u %u\n", img->width, img->height);
+
+    size_t data_size = (img->width * img->height + 7) / 8;
+    if (fwrite(img->data, 1, data_size, file) != data_size) {
+        fclose(file);
+        return false;
+    }
+
+    fclose(file);
+    return true;
+}
+
+static ImageNpbm* create_white_image(uint32_t width, uint32_t height,
+                                     Netpbm type) {
+    uint8_t channels;
+    size_t precision;
+    size_t data_size;
+
+    switch (type) {
+        case PBM:
+            channels = 1;
+            precision = 1;
+            data_size = (width * height + 7) / 8;
+            break;
+        case PGM:
+            channels = 1;
+            precision = 255;
+            data_size =
+                width * height * channels * bytes_per_channel(precision);
+            break;
+        case PPM:
+            channels = 3;
+            precision = 255;
+            data_size =
+                width * height * channels * bytes_per_channel(precision);
+            break;
+        case PAM:
+            channels = 4;
+            precision = 255;
+            data_size =
+                width * height * (channels + 1) * bytes_per_channel(precision);
+            break;
+        default:
+            return NULL;
+    }
+
+    ImageNpbm* img = new_image(width, height, channels, precision, type);
+    if (!img) return NULL;
+
+    memset(img->data, (type == PBM) ? 0xff : precision, data_size);
+
+    return img;
 }
 
 #ifdef __cplusplus
--- main.c
+++ main.c
@@ -1,3 +1,20 @@
 #include "image.h"
 
-int main() {  }
+int main() {
+    uint32_t width = 100;
+    uint32_t height = 100;
+
+    ImageNpbm* white_image = create_white_image(width, height, PBM);
+    if (!white_image) {
+        LOG("Failed to create white PBM image\n");
+        return 1;
+    }
+
+    if (!write_pbm_image("white_image.pbm", white_image)) {
+        LOG("Failed to write white PBM image to file\n");
+        return 1;
+    }
+
+    free_image(white_image);
+    return 0;
+}
