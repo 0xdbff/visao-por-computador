642e6283a77d8fed89357452461c097dfffb91a1
db <diogo.bernardo.dev@gmail.com>
2023-05-17 11:08:07 +0100

    sync locals

--- main.cpp
+++ main.cpp
@@ -1,69 +1,91 @@
 #include <opencv2/opencv.hpp>
 
-std::vector<cv::Rect> detectTrafficSigns(const cv::Mat& image) {
-    std::vector<cv::Rect> signs;
+std::pair<std::vector<cv::Rect>, cv::Mat> detectRedRegions(const cv::Mat& image) {
+    std::vector<cv::Rect> redRegions;
 
-    // Convert the image to grayscale
-    cv::Mat gray;
-    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
-
-    // Threshold the image
-    cv::Mat thresh;
-    cv::threshold(gray, thresh, 100, 255, cv::THRESH_BINARY);
-
-    // Find contours in the thresholded image
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(thresh, contours, cv::RETR_EXTERNAL,
-                     cv::CHAIN_APPROX_SIMPLE);
-
-    // Filter contours based on size and shape
-    for (const auto& contour : contours) {
-        cv::Rect rect = cv::boundingRect(contour);
-        double aspectRatio = static_cast<double>(rect.width) / rect.height;
-
-        if (cv::contourArea(contour) > 1000 && aspectRatio > 0.7 &&
-            aspectRatio < 1.3) {
-            signs.push_back(rect);
-        }
-    }
-
-    return signs;
-}
-std::string classifyTrafficSign(const cv::Mat& signImage) {
     // Convert the image to the HSV color space
     cv::Mat hsv;
-    cv::cvtColor(signImage, hsv, cv::COLOR_BGR2HSV);
+    cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
 
-    // Threshold the HSV image to get only red colors
+    // Threshold the HSV image for only red colors
     cv::Mat mask1, mask2;
-    cv::inRange(hsv, cv::Scalar(0, 70, 50), cv::Scalar(10, 255, 255), mask1);
-    cv::inRange(hsv, cv::Scalar(170, 70, 50), cv::Scalar(180, 255, 255), mask2);
+    cv::inRange(hsv, cv::Scalar(0, 140, 90), cv::Scalar(10, 255, 255), mask1);
+    cv::inRange(hsv, cv::Scalar(170, 140, 90), cv::Scalar(180, 255, 255), mask2);
 
     // Combine the above two masks to get our final mask
     cv::Mat mask = mask1 | mask2;
 
-    // Find contours in the mask
+    // Perform morphological operations
+    cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(8, 8));
+    cv::erode(mask, mask, element);
+    cv::dilate(mask, mask, element);
+
+    // Find contours in the thresholded image
     std::vector<std::vector<cv::Point>> contours;
     cv::findContours(mask, contours, cv::RETR_EXTERNAL,
                      cv::CHAIN_APPROX_SIMPLE);
+
+    // Filter contours based on size
+    for (const auto& contour : contours) {
+        cv::Rect rect = cv::boundingRect(contour);
+        double area = cv::contourArea(contour);
+
+        if (area > 1000 && area < 50000) {  // Adjust these values as necessary
+            redRegions.push_back(rect);
+        }
+    }
+
+    return std::make_pair(redRegions, mask);
+}
+
+std::string classifyTrafficSign(const cv::Mat& signImage) {
+    // Convert to grayscale
+    cv::Mat gray;
+    cv::cvtColor(signImage, gray, cv::COLOR_BGR2GRAY);
+
+    // Blur the image
+    cv::GaussianBlur(gray, gray, cv::Size(5, 5), 0);
+
+    // Use Canny edge detection
+    cv::Mat edges;
+    cv::Canny(gray, edges, 50, 150);
+
+    // Find contours in the edge image
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(edges, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
 
     // Filter contours based on size and shape
     for (const auto& contour : contours) {
         // Approximate the contour to a polygon
         std::vector<cv::Point> approx;
-        cv::approxPolyDP(contour, approx, cv::arcLength(contour, true) * 0.02,
-                         true);
+        cv::approxPolyDP(contour, approx, cv::arcLength(contour, true) * 0.02, true);
 
-        // If the polygon has 8 sides, it might be a stop sign
-        if (approx.size() == 8) {
+        // Calculate the area and perimeter of the contour
+        double area = cv::contourArea(contour);
+        double perimeter = cv::arcLength(contour, true);
+
+        // Calculate circularity
+        double circularity = 4 * CV_PI * area / (perimeter * perimeter);
+
+        // If the polygon is close to circular, it might be a 'No Entry' or 'Stop' sign
+        if (circularity > 0.85) {  // Adjust this value as necessary
+            // Check for the presence of a horizontal line for 'No Entry' sign
+            std::vector<cv::Vec4i> lines;
+            cv::HoughLinesP(edges, lines, 1, CV_PI/180, 50, 50, 10);
+            for (const auto& line : lines) {
+                if (abs(line[1] - line[3]) < 20) {
+                    return "No Entry";
+                }
+            }
+
+            // If no horizontal line is found, it could be a stop sign
             return "Stop";
         }
     }
 
-    // If no stop sign is detected, return an empty string
-    return "Unrecognized";
+    // If no sign is detected, return an empty string
+    return "";
 }
-
 
 int main() {
     cv::VideoCapture cap(0);
@@ -76,18 +98,21 @@ int main() {
     while (true) {
         cap >> frame;
 
-        // // Detect traffic signs
-        std::vector<cv::Rect> signs = detectTrafficSigns(frame);
+        // Detect red regions
+        auto [redRegions, mask] = detectRedRegions(frame);
 
-        // Classify each sign
-        for (const cv::Rect& sign : signs) {
-            cv::Mat signImage = frame(sign);
-            std::string label = classifyTrafficSign(signImage);
+        cv::imshow("Mask", mask);
 
-            // Draw bounding box and label
-            cv::rectangle(frame, sign, cv::Scalar(0, 255, 0));
-            cv::putText(frame, label, sign.tl(), cv::FONT_HERSHEY_SIMPLEX, 1.0,
-                        cv::Scalar(255, 255, 255));
+        // Classify each region
+        for (const cv::Rect& region : redRegions) {
+            cv::Mat regionImage = frame(region);
+            std::string label = classifyTrafficSign(regionImage);
+
+            // Draw bounding box and label only if a sign is detected
+            if (!label.empty()) {
+                cv::rectangle(frame, region, cv::Scalar(0, 255, 0));
+                cv::putText(frame, label, region.tl(), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255));
+            }
         }
 
         // Display the frame
@@ -97,4 +122,8 @@ int main() {
             break;
         }
     }
+
+    cv::destroyWindow("Mask");
+
+    return 0;
 }
