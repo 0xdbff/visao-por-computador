a0ea35b4c1075a9c4455d32abfa8062db530ee74
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-06-08 00:30:32 +0100

    sync locals

--- main.cpp
+++ main.cpp
@@ -1,229 +1,198 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <ostream>
 
-std::pair<std::vector<cv::Rect>, cv::Mat> detectRedRegions(
-    const cv::Mat& image) {
-    std::vector<cv::Rect> redRegions;
+#define RED_LOWER_BOUND1 cv::Scalar(0, 130, 80)
+#define RED_UPPER_BOUND1 cv::Scalar(10, 255, 255)
+#define RED_LOWER_BOUND2 cv::Scalar(165, 130, 80)
+#define RED_UPPER_BOUND2 cv::Scalar(180, 255, 255)
 
-    // Convert the image to the HSV color space
-    cv::Mat hsv;
-    cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+#define BLUE_LOWER_BOUND cv::Scalar(104, 110, 80)
+#define BLUE_UPPER_BOUND cv::Scalar(124, 255, 255)
 
-    // Threshold the HSV image for only red colors
-    cv::Mat mask1, mask2;
-    cv::inRange(hsv, cv::Scalar(0, 140, 90), cv::Scalar(6, 255, 255), mask1);
-    cv::inRange(hsv, cv::Scalar(164, 140, 90), cv::Scalar(180, 255, 255),
-                mask2);
+#define CIRCLE_DETECTION_PARAM1 30
+#define CIRCLE_DETECTION_PARAM2 20
+#define MIN_RADIUS 20
+#define MAX_RADIUS 600
+#define OCTAGON_APPROXIMATION_PARAM 0.02
+#define OCTAGON_CIRCULARITY_THRESHOLD 0.65
 
-    // Combine the above two masks to get our final mask
-    cv::Mat mask = mask1 | mask2;
+class ColorDetector {
+   public:
+    static cv::Mat detectRed(const cv::Mat& img) {
+        cv::Mat hsv;
+        cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
 
-    // Perform morphological operations
-    cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 6));
-    cv::erode(mask, mask, element);
-    cv::dilate(mask, mask, element);
+        cv::Mat mask1, mask2;
+        cv::inRange(hsv, RED_LOWER_BOUND1, RED_UPPER_BOUND1, mask1);
+        cv::inRange(hsv, RED_LOWER_BOUND2, RED_UPPER_BOUND2, mask2);
+        cv::Mat mask = mask1 | mask2;
 
-    // Find contours in the thresholded image
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(mask, contours, cv::RETR_EXTERNAL,
-                     cv::CHAIN_APPROX_SIMPLE);
+        // Add noise reduction
+        cv::Mat kernel =
+            cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+        cv::morphologyEx(mask, mask, cv::MORPH_OPEN, kernel, cv::Point(-1, -1),
+                         2);
+        cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, kernel, cv::Point(-1, -1),
+                         2);
 
-    // Filter contours based on size
-    for (const auto& contour : contours) {
-        cv::Rect rect = cv::boundingRect(contour);
-        double area = cv::contourArea(contour);
-
-        if (area > 1000 && area < 50000) {  // Adjust these values as necessary
-            redRegions.push_back(rect);
-        }
+        return mask;
     }
 
-    return std::make_pair(redRegions, mask);
-}
+    static cv::Mat detectBlue(const cv::Mat& img) {
+        cv::Mat hsv;
+        cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
 
-std::pair<std::vector<cv::Vec3f>, cv::Mat> detectBlueCircles(
-    const cv::Mat& image) {
-    std::vector<cv::Vec3f> circles;
+        cv::Mat mask;
+        cv::inRange(hsv, BLUE_LOWER_BOUND, BLUE_UPPER_BOUND, mask);
 
-    // Convert the image to the HSV color space
-    cv::Mat hsv;
-    cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+        // Add noise reduction
+        cv::Mat kernel =
+            cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+        cv::morphologyEx(mask, mask, cv::MORPH_OPEN, kernel, cv::Point(-1, -1),
+                         2);
+        cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, kernel, cv::Point(-1, -1),
+                         2);
 
-    // Threshold the HSV image for only blue colors
-    cv::Mat mask;
-    cv::inRange(hsv, cv::Scalar(100, 50, 50), cv::Scalar(140, 255, 255), mask);
-
-    // Perform morphological operations
-    cv::Mat element =
-        cv::getStructuringElement(cv::MORPH_RECT, cv::Size(15, 15));
-    cv::erode(mask, mask, element);
-    cv::dilate(mask, mask, element);
-
-    // Apply Hough Circle Transform to find circles
-    cv::HoughCircles(mask, circles, cv::HOUGH_GRADIENT, 1,
-                     mask.rows / 8,  // Change this value as needed
-                     100, 30,        // Change the two thresholds as needed
-                     0, 0);          // Change the min and max radius as needed
-
-    return std::make_pair(circles, mask);
-}
-
-std::map<std::string, cv::Mat> loadTemplates() {
-    std::map<std::string, cv::Mat> templates;
-    std::string signs[] = {"ArrowLeft", "ArrowRight", "Car",
-                           "Forbidden", "Highway",    "Stop"};
-    for (const auto& sign : signs) {
-        cv::Mat templateImage =
-            cv::imread("../sinais/" + sign + ".jpg", cv::IMREAD_GRAYSCALE);
-
-        std::cout << "load template for: " << sign << std::endl;
-
-        if (templateImage.empty()) {
-            std::cout << "Failed to load template for: " << sign << std::endl;
-            continue;
-        }
-
-        cv::Mat mask = templateImage.clone();
-        cv::threshold(
-            mask, mask, 1, 255,
-            cv::THRESH_BINARY);  // Any value greater than 1 becomes 255
-        templates[sign] = templateImage;
-        templates[sign + "_mask"] =
-            mask;  // Store the mask with a key postfixed with "_mask"
+        return mask;
     }
-    return templates;
-}
+};
 
-std::string matchTemplateWithSigns(const cv::Mat& signImage,
-                                   std::map<std::string, cv::Mat>& templates) {
-    std::string bestMatch;
-    double maxMatch = 0;
-    for (const auto& temp : templates) {
-        // Skip the masks in the templates map
-        if (temp.first.find("_mask") != std::string::npos) {
-            continue;
-        }
-        // Check if the signImage is larger than the template image
-        // if (signImage.rows < temp.second.rows ||
-        //     signImage.cols < temp.second.cols) {
-        //     continue;
-        // }
+class ShapeDetector {
+   public:
+    static std::vector<cv::Vec3f> detectCircles(const cv::Mat& img) {
+        std::vector<cv::Vec3f> circles;
+        cv::HoughCircles(img, circles, cv::HOUGH_GRADIENT, 1, img.rows / 8,
+                         CIRCLE_DETECTION_PARAM1, CIRCLE_DETECTION_PARAM2,
+                         MIN_RADIUS, MAX_RADIUS);
 
-        cv::Mat result;
-        cv::Mat mask =
-            templates[temp.first + "_mask"];  // Get the corresponding mask
-        cv::matchTemplate(signImage, temp.second, result, cv::TM_CCORR_NORMED,
-                          mask);
-        double minVal, maxVal;
-        cv::minMaxLoc(result, &minVal, &maxVal);
+        std::vector<cv::Vec3f> filtered_circles;
+        for (const auto& circle : circles) {
+            cv::Mat circleMask = cv::Mat::zeros(img.rows, img.cols, CV_8U);
+            cv::circle(circleMask, cv::Point(circle[0], circle[1]), circle[2],
+                       cv::Scalar(255), -1);
 
-        if (maxVal > maxMatch) {
-            maxMatch = maxVal;
-            bestMatch = temp.first;
-        }
-    }
-    if (maxMatch > 0.85) {  // 0.75 is a threshold, you can adjust it according
-        std::cout << "Match for " << bestMatch << ": " << maxMatch << std::endl;
-        return bestMatch;
-    } else {
-        return "";
-    }
-}
+            double expectedArea = CV_PI * circle[2] * circle[2];
+            double actualArea = cv::countNonZero(img & circleMask);
 
-std::string classifyTrafficSign(const cv::Mat& signImage,
-                                std::map<std::string, cv::Mat>& templates) {
-    cv::Mat signImageGray = signImage.clone();
-    cv::cvtColor(signImageGray, signImageGray, cv::COLOR_BGR2GRAY);
-
-    for (const auto& temp : templates) {
-        // Skip the masks in the templates map
-        if (temp.first.find("_mask") != std::string::npos) {
-            continue;
+            if (actualArea / expectedArea > 0.4) {
+                filtered_circles.push_back(circle);
+            }
         }
 
-        if (signImageGray.rows < temp.second.rows ||
-            signImageGray.cols < temp.second.cols) {
-            // Resize signImageGray to the size of template
-            cv::resize(signImageGray, signImageGray, temp.second.size());
-        }
+        return filtered_circles;
     }
 
-    return matchTemplateWithSigns(signImageGray, templates);
-}
+    static std::vector<std::vector<cv::Point>> detectOctagons(
+        const cv::Mat& img) {
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(img, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+        std::vector<std::vector<cv::Point>> octagons;
+        for (const auto& contour : contours) {
+            std::vector<cv::Point> approx;
+            cv::approxPolyDP(
+                contour, approx,
+                cv::arcLength(contour, true) * OCTAGON_APPROXIMATION_PARAM,
+                true);
+
+            if (approx.size() == 8) {
+                double maxEdgeLength = 0.0;
+                double minEdgeLength = std::numeric_limits<double>::max();
+                for (int i = 0; i < approx.size(); i++) {
+                    cv::Point diff =
+                        approx[(i + 1) % approx.size()] - approx[i];
+                    double edgeLength =
+                        cv::sqrt(diff.x * diff.x + diff.y * diff.y);
+                    minEdgeLength = std::min(minEdgeLength, edgeLength);
+                    maxEdgeLength = std::max(maxEdgeLength, edgeLength);
+                }
+                double edgeLengthRatio = minEdgeLength / maxEdgeLength;
+                if (edgeLengthRatio < 0.8) {  // adjust this threshold as needed
+                    continue;  // discard the contour if the edge lengths vary
+                               // too much
+                }
+
+                octagons.push_back(approx);
+            }
+        }
+
+        return octagons;
+    }
+};
 
 int main() {
-    std::cout << "Starting the program..." << std::endl;
-
     cv::VideoCapture cap(0);
-
     if (!cap.isOpened()) {
-        std::cerr << "Error: Video capture is not opened." << std::endl;
         return -1;
     }
 
-    std::cout << "Loading templates..." << std::endl;
-    std::map<std::string, cv::Mat> templates = loadTemplates();
+    const double overlapThreshold = 0.1;
 
     cv::Mat frame;
+    while (cap.read(frame)) {
+        cv::Mat hsv;
+        cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
 
-    while (true) {
-        cap >> frame;
+        // Split the image into H, S, and V channels
+        std::vector<cv::Mat> hsv_channels;
+        cv::split(hsv, hsv_channels);
+        // Apply histogram equalization to the V channel
+        cv::equalizeHist(hsv_channels[2], hsv_channels[2]);
+        // Merge the H, S, and V channels back into a single image
+        cv::merge(hsv_channels, hsv);
+        // Convert back to BGR color space for color detection
+        cv::cvtColor(hsv, frame, cv::COLOR_HSV2BGR);
 
-        auto [redRegions, redMask] = detectRedRegions(frame);
-        auto [blueCircles, blueMask] = detectBlueCircles(frame);
+        cv::Mat redMask = ColorDetector::detectRed(frame);
+        cv::Mat blueMask = ColorDetector::detectBlue(frame);
+        cv::Mat colorMask = redMask | blueMask;
 
-        cv::Mat combinedMask = redMask | blueMask;
-        cv::cvtColor(combinedMask, combinedMask, cv::COLOR_GRAY2BGR);
+        std::vector<cv::Vec3f> circles =
+            ShapeDetector::detectCircles(colorMask);
+        std::vector<std::vector<cv::Point>> octagons =
+            ShapeDetector::detectOctagons(redMask);
 
-        for (const cv::Rect& region : redRegions) {
-            if (region.width <= 0 || region.height <= 0) {  // Add this check
-                continue;
-            }
-            cv::Mat regionImage = frame(region);
+        for (const auto& circle : circles) {
+            if (circle[2] < 20 || circle[2] > 500) continue;
 
-            cv::imshow("Region", regionImage);
-
-            std::string label = classifyTrafficSign(regionImage, templates);
-
-            if (!label.empty()) {
-                std::cout << "Red Match Found: " << label << " at " << region.x
-                          << "," << region.y << std::endl;
-                cv::rectangle(frame, region, cv::Scalar(0, 255, 0));
-                cv::putText(frame,
-                            label + " at " + std::to_string(region.x) + "," +
-                                std::to_string(region.y),
-                            region.tl(), cv::FONT_HERSHEY_SIMPLEX, 1.0,
-                            cv::Scalar(255, 255, 255));
-            }
-        }
-
-        for (const cv::Vec3f& circle : blueCircles) {
-            cv::Point center(cvRound(circle[0]), cvRound(circle[1]));
-            int radius = cvRound(circle[2]);
-            cv::circle(frame, center, radius, cv::Scalar(255, 0, 0), 3, 8, 0);
-
-            cv::Mat regionImage = frame(cv::Rect(
-                center.x - radius, center.y - radius, 2 * radius, 2 * radius));
-
-            cv::imshow("Region", regionImage);
-
-            if (!regionImage.empty()) {
-                std::string label = classifyTrafficSign(regionImage, templates);
-
-                if (!label.empty()) {
-                    std::cout << "Blue Match Found: " << label << " at "
-                              << center.x << "," << center.y << std::endl;
-                    cv::putText(frame,
-                                label + " at " + std::to_string(center.x) +
-                                    "," + std::to_string(center.y),
-                                center, cv::FONT_HERSHEY_SIMPLEX, 1.0,
-                                cv::Scalar(255, 255, 255));
+            cv::Rect circleRect(circle[0] - circle[2], circle[1] - circle[2],
+                                2 * circle[2], 2 * circle[2]);
+            for (const auto& octagon : octagons) {
+                cv::Rect octagonRect = cv::boundingRect(octagon);
+                double intersectionArea = (circleRect & octagonRect).area();
+                double unionArea =
+                    circleRect.area() + octagonRect.area() - intersectionArea;
+                double overlap = intersectionArea / unionArea;
+                if (overlap > overlapThreshold) {
+                    continue;  // skip this circle if it overlaps too much with
+                               // an octagon
                 }
             }
+
+            cv::circle(frame, cv::Point(circle[0], circle[1]), circle[2],
+                       cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+            cv::putText(frame, "Circle", cv::Point(circle[0], circle[1]),
+                        cv::FONT_HERSHEY_SIMPLEX, 0.5,
+                        cv::Scalar(255, 255, 255), 2);
         }
 
-        cv::imshow("Analysis", frame);
-        cv::imshow("Mask", combinedMask);
+        for (const auto& octagon : octagons) {
+            cv::polylines(frame, octagon, true, cv::Scalar(0, 255, 0), 3,
+                          cv::LINE_AA);
+
+            // Calculate the center of the contour
+            cv::Moments M = cv::moments(octagon);
+            int cX = static_cast<int>(M.m10 / M.m00);
+            int cY = static_cast<int>(M.m01 / M.m00);
+
+            cv::putText(frame, "Octagon", cv::Point(cX, cY),
+                        cv::FONT_HERSHEY_SIMPLEX, 0.5,
+                        cv::Scalar(255, 255, 255), 2);
+        }
+
+        cv::imshow("frame", frame);
+        cv::imshow("color", colorMask);
 
         int key = cv::waitKey(30);
         if (key == 'x' || key == 'X') {
